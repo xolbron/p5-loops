1a5c9c291c9ba447304933287fc4e93a5450bccb
Jerome Foster II <jfoster@wlapcs.org>
2017-03-30 22:05:04 +0000

    made it cooler

--- sketch.js
+++ sketch.js
@@ -3,22 +3,23 @@ var pg;
 
 
 function setup() {
-    createCanvas(710,400);
+    createCanvas(1352,610);
     stroke(255);
     frameRate(30);
     background(0);
     pg = createGraphics(400, 250);
     for(var x = 0; x < width; x = x + 35){
-      ellipse(x, 200, 25, 30);
+      ellipse(x, 500, 25, 3, random(255));
   }
 }
 function draw(){
   strokeWeight(4);
   stroke(255);
-  frameRate(10);
+  frameRate(198);
   var x = 0;
   for(var x = 0; x < width; x = x + 35){
-      ellipse(x, 200, 25, random(400));
+      fill(random(255), random(255), random(255));
+      ellipse(x, 300, 25, random(550));
   }
   fill(0, 59, 155);
   y = y - 5; 
